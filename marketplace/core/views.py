f6d4f45ebccb0dba75b9224ccb653f33539850f0
ShabinMuhammedKK <shabinmuhammedkk@gmail.com>
2024-11-14 19:44:06 +0530

    initial commit

---
+++ marketplace/core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from item.models import Category, Item
+
+
+def index(request):
+    item = Item.objects.filter(is_sold=False)[0:6]
+    category = Category.objects.all()
+    return render(request, "core/index.html", {"items": item, "categorys": category})
+
+
+def contact(request):
+    return render(request, "core/contact.html")
